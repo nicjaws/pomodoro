25abf28d83976d3c0bce4e61450a553fccaf6916
Nicolas Corbalan <outreef@hotmail.com>
2020-07-16 17:16:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import './App.css';
+
+export default function App() {
+  const [ timeLeft, setTimeLeft] = useState(25 * 60)
+
+  return (
+    <div className="app">
+      <h2>Pomodoro!</h2>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
+}
